7218337cfc92da829d894eeecc3f4e06b8c93ff4
rajatsinha05 <rajatsinha5467@gmail.com>
2025-01-29 18:59:14 +0530

    sql

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./config/db");
 (async () => {
   try {
@@ -18,7 +19,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 // app.use("/api/v1/users",userRouter)
-
+app.use(cors());
 app.use("/api/v1", appRoutes);
 
 // listen
--- models/user.js
+++ models/user.js
@@ -3,13 +3,12 @@ const db = require("../config/db");
 const { ulid } = require("ulid");
 
 const User = db.define("User", {
-  id: { type: STRING, primaryKey: true, defaultValue: () => ulid() },
+  id: { type: INTEGER, primaryKey: true, autoIncrement: true },
   username: { type: STRING },
   email: { type: STRING, unique: true, allowNull: false },
   password: { type: STRING, allowNull: false },
 });
 
 module.exports = User;
-
 
 // console.log(ulid());
